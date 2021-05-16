a37840918c3a9f2de714a7a615b971a21fe7c209
Jairo Master Zion <master.zion@gmail.com>
2021-05-16 15:22:04 +0200

    bugfixes

--- mzstream.sh
+++ mzstream.sh
@@ -9,7 +9,7 @@ export TOP_POS_CAM=24
 export LEFT_POS_CAM=900
 
 
-ffmpeg -i /dev/video2 -i /dev/video4 -f alsa -i hw:$3 -filter_complex "
+ffmpeg -i $CAPTURECARD -i $WEBCAMSRC -f alsa -i hw:$3 -filter_complex "
 [0:v]setpts=PTS-STARTPTS,scale=$RESOLUTION:[left];
 [1:v]setpts=PTS-STARTPTS,scale=260x150:[right];
 [left][right]overlay=shortest=1:x=750:y=20[v]" -map "[v]" -map  "2:a" \
