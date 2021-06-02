b7dba5144d06a2aa701f993d3b3371af88c5db6e
Jairo Moreno <master.zion@gmail.com>
2021-06-02 01:55:11 +0100

    audio sync

--- mzstream.sh
+++ mzstream.sh
@@ -11,15 +11,20 @@ export LEFT_POS_CAM=900
 
 while :
 do
-   ffmpeg -i $CAPTURECARD -vsync 0 -i $WEBCAMSRC -f alsa -thread_queue_size 2048 -i hw:$3 -filter_complex "
-   [0:v]setpts=PTS-STARTPTS:,scale=1024:-1[left];
-   [1:v]setpts=PTS-STARTPTS,scale=260x150:[right];
+   ffmpeg \
+    -ss 0.5 -i $WEBCAMSRC \
+    -ss 5 -i $CAPTURECARD \
+    -f alsa  -thread_queue_size 8K -i hw:$3 \
+    -filter_complex "
+   [1:v]setpts=PTS-STARTPTS:,scale=1024:-1[left];
+   [0:v]setpts=PTS-STARTPTS,scale=260x150:[right];
    [left][right]overlay=shortest=1:x=750:y=20[v]" -map "[v]" -map  "2:a"  \
-      -threads 8 -preset ultrafast -maxrate 1500k -bufsize 64000k   \
+      -preset ultrafast -maxrate 2000k -bufsize 64000k  \
       -acodec libmp3lame  -q:a 1   \
-      -pix_fmt yuv420p  -vcodec libx264  -f flv \
+      -pix_fmt yuv420p  -vcodec libx264   \
+      -f flv -flags:v +global_header -rtmp_buffer 10000 -r 30 -async 1 \
       ${SERVER}${TWITCH_KEY}
-
+ exit
    echo "Stream error..."
    sleep 3
    echo "Retrying..."
