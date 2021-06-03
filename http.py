aa2fe2dd5298a5e7b0dd6d5d0e8f29acbe3e9636
Jairo Master Zion <master.zion@gmail.com>
2021-06-03 20:41:32 +0200

    add port as parameter, readable config json

--- http.py
+++ http.py
@@ -1,8 +1,14 @@
 import SimpleHTTPServer
 import SocketServer
 import os
+import sys
+
+if len(sys.argv) > 1:
+    PORT = int(sys.argv[1])
+else:
+    PORT = 8082
+
 os.chdir("web")
-PORT = 8081
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_POST(self):
@@ -10,19 +16,29 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         post_body = self.rfile.read(content_len)
         if post_body == "0":
             os.remove("run.txt")
+            self.send_response(200)
         else:
             file1 = open("config.json","w")
+            post_body = post_body.replace("{", "\n{\n")
+            post_body = post_body.replace("}", "\n}\n")
+            post_body = post_body.replace(",", ",\n")
+
             file1.write(post_body)
             file1.close()
 
             file1 = open("run.txt","w")
             file1.write("1")
             file1.close()
+            self.send_response(200)
 
 
 Handler = ServerHandler
 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-print "serving at port", PORT
-httpd.serve_forever()
+print ("serving at port", PORT)
+
+try:
+	httpd.serve_forever()
+except KeyboardInterrupt:
+	pass
